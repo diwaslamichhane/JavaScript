55da63cc6a3ec710df6316b2597e563eba4e903f
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-11 23:07:26 +0545

    Understanding Primitive Type In JS
    Understanding Primitive Type In JS

---
+++ PrimitiveTypeInJS.js
@@ -0,0 +1,9 @@
+// *primitive = number, string, null, undefined, boolean
+
+// Example of primitive
+
+var a = 10;
+var a = b;
+console.log(b);
+
+// When we try to copy the value of a and if b copies the real value then it is primitive value. 
