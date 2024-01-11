c0ffc2a3f286c829407531f6ca5ed333bc20e644
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-11 22:45:30 +0545

    Understanding Variables and Constant in JS
    Understanding Variables and Constant in JS

---
+++ VariablesAndConstant.js
@@ -0,0 +1,12 @@
+// Understanding Variables and Constant in JS 
+var a = 1;
+console.log(a);
+
+var b = "Diwas";
+console.log(b);
+
+let c = 2;
+console.log(c);
+
+const sun = "Rises From East";
+console.log(sun);
