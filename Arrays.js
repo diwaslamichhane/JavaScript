2b6e171521b0718d15ea9e944fae3ede47e90c64
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-14 17:10:14 +0545

    Understanding Arrays in JS
    Understanding Arrays in JS

---
+++ Arrays.js
@@ -0,0 +1,9 @@
+// Understanding Arrays in JS
+
+// Arrays are used to store more than one value in a variable. For Example: 
+
+var a = [1, 2, 3, 4, 5];
+// index means counting, it starts from 0.
+// i.e. index 0 = 1,
+//  index 1 = 2,
+//   index 2 = 3 and so on ......
