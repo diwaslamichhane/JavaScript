feaae8c0ffc406729ae3c718ab88e5fa2465e076
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-14 16:53:19 +0545

    Understanding Function in JS
    Understanding Function in JS

---
+++ Function.js
@@ -0,0 +1,12 @@
+// Understanding function in JS 
+
+// function are used to run our code whenever we want at our desired time. Simply function means to give name to our code. For EG:
+
+// console.log("Hello Coders!"); This runs our code instantly.
+
+// BUT using function,
+function abcd(){
+    console.log("Hello Coders!");
+}
+
+abcd(); //we should call function name when we want to print it.
