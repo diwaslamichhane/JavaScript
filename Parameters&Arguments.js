41af2b71d61d19d07320b3c11bd1dfd086114233
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-14 17:00:16 +0545

    Understanding Parameters & Arguments in JS
    Understanding Parameters & Arguments in JS

---
+++ Parameters&Arguments.js
@@ -0,0 +1,12 @@
+// Understanding Parameters & Arguments in JS 
+
+// parameters means variables to store values just like var a, let b, when using a function.
+// arguments means values to store on those variables when using function
+
+// Example
+
+function abcd(a /* This is parameter */){
+console.log(a);
+}
+
+abcd(5 /*This is argument */);
