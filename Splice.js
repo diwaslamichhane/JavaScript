f41d82818f4814b6618b7c24a38e29bf8122f3bc
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-15 13:07:17 +0545

    Understanding .splice(index, n)
    Understanding .splice(index, n)

---
+++ Splice.js
@@ -0,0 +1,6 @@
+// Understanding .splice(index, n)
+
+// Splice: to remove element from the between. Example:
+var arr = [1, 2, 3, 4, 5, 6, 7]
+// to remove 3, count index of 3 first and keep it in splice then count how many elements to remove, if 1 element then write .splice(index, 1) if 2 element then write .splice(index, 2)
+arr.splice(2, 1); //3 will be removed from the array
