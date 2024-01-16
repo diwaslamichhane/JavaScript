8c4ad43726e6db519c2d171afae48569e8568ede
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-16 20:01:40 +0545

    Create Objects.js

---
+++ Objects.js
@@ -0,0 +1,18 @@
+// Understanding Objects in JS
+
+//Objects are used to store details of an item. Example:
+let fruit = {
+    name: "Apple",
+    place: "Mustang",
+    price: "RS180 per KG",
+    taste: "Sweet"
+}
+// this is an object
+// Now, to access we write,
+// objectname.property EXAMPLE:
+
+fruit.taste //'sweet' will be printed
+
+//we can also change the value, EXAMPLE:
+fruit.name = "Orange";
+// name will be changed into Orange now. 
