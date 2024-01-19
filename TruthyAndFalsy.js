26cfa12c55bb031e30af70043db0800034c449d8
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-19 16:46:47 +0545

    Update TruthyAndFalsy.js

--- TruthyAndFalsy.js
+++ TruthyAndFalsy.js
@@ -9,3 +9,11 @@ Falsy Values are: 0, false, undefined, null, NaN, document.all
 Except above values, all others are truthy values
 
 */
+
+// Example of Truthy
+if(69){
+    console.log("Since it is truthy, this will print!");
+}
+else{
+    console.log("Since it is falsy, this will run!")
+}
