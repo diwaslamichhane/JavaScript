0444090c88116cb1bfc8c6fd89f6428e33c6cdbf
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-19 16:39:08 +0545

    How to copy reference value
    How to copy reference value

--- CopyReferenceValue.js
+++ CopyReferenceValue.js
@@ -5,3 +5,11 @@ var a = [1, 2, 3, 4, 5, 6, 7, 8];
 var b = [...a]; // [...] 3dots
 
 // Now when we change the value of b, the value of a will remain same 
+
+// we can do it in object also:
+var obj = {
+    name: "Diwas",
+    age: 20
+};
+
+var cpyObj = {...obj};
