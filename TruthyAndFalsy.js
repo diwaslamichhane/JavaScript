44ccae7407594cc4450effa149f5d3a8783a3e67
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-19 16:46:24 +0545

    Create TruthyAndFalsy.js

---
+++ TruthyAndFalsy.js
@@ -0,0 +1,11 @@
+// Understanding Truthy and Falsy
+
+/*
+
+whatever we write in javascript, it belongs to either Truthy or Falsy
+
+Falsy Values are: 0, false, undefined, null, NaN, document.all
+
+Except above values, all others are truthy values
+
+*/
