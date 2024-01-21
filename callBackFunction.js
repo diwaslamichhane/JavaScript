e74e06f0677aeef86fa556596dd36560eda22563
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-21 17:50:03 +0545

    Create callBackFunction.js

---
+++ callBackFunction.js
@@ -0,0 +1,15 @@
+// Understanding call back function 
+
+/*
+
+A function in which code is written which runs later is called call back function.
+
+Example:
+
+*/
+
+setTimeout(function(){
+    console.log("This will print after 5 seconds.");
+}, 5000);
+
+//setTimeout( function() {CODE}, Time);
