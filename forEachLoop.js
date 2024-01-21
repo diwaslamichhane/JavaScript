5e4975af0327ed478c138a4895a83deb00ac5d74
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-21 17:48:25 +0545

    Understanding forEach Loops
    Understanding forEach Loops

--- forEachLoop.js
+++ forEachLoop.js
@@ -8,3 +8,8 @@ If you want to modify each elements the forEach loop is used.
 Example:
 
 */
+
+let a = [1, 2, 3, 4, 5];
+a.forEach(function(val){
+    console.log(val+2); //2 will be added in every elements of the array
+});
