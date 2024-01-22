5f09d115c56575dfc3f32b340c7a8c58086fba21
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-22 19:23:30 +0545

    Understanding First Class Function
    Understanding First Class Function

---
+++ FirstClassFunction.js
@@ -0,0 +1,14 @@
+// Understanding First Class Function 
+
+// It is a concept where function is treated as a value or we can use function as a value. Example:-
+
+var a = function abcd(){
+
+};
+
+//or
+
+function abcd(value){
+
+}
+abcd(function(){console.log("Hey");})
