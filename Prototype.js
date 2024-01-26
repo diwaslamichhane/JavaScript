54bf0719d6ef863de26f79e25ebff818e1d13df9
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-26 20:46:55 +0545

    Understanding Prototype
    Understanding Prototype

---
+++ Prototype.js
@@ -0,0 +1,23 @@
+// Understanding Prototype
+
+/*
+
+when we create an Object or an Array, it contains a default property called prototype. 
+
+1) open your console and create an object.
+
+var obj = {
+    name: "Diwas"
+}
+
+2) then type obj and dot, obj. in console
+
+3) after typing obj. you will get to name (which we have written in object) and many other thing which we have not written, they are PROTOTYPE.
+
+4) Every created objects and arrays in javascript contains prototypes by default.
+
+*/
+
+var obj = {
+    name: "Diwas"
+}
