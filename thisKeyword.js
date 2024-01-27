b8a39a2da54ce5bf21254c93d23ababb86e6f9c1
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-27 21:52:28 +0545

    Update thisKeyword.js

--- thisKeyword.js
+++ thisKeyword.js
@@ -21,3 +21,38 @@ function abcd(){
 
 console.log(this); //gives window
 
+// In function scope 
+
+function abcd(){
+    console.log(this); //gives window
+}
+abcd()
+
+// In method scope 
+// Function which is inside a object is called method
+
+var obj = {
+    name: "Diwas",
+    thisIsMethod: function(){
+        console.log(this); //gives object
+    }
+}
+obj.thisIsMethod(); 
+
+// NOTE: In any method 'this' keyword always refers to parent Object.   i.e. in upper method case this gave object which refers to its parent object, that is var obj 
+
+// In event listner 
+
+var button = document.querySelector("button");
+button.addEventListener("click", function (){
+    console.log(this);  //gives whatever infront of .addEventListner. In this case it is button.
+});
+
+/*
+Conclusion
+
+In global scope: gives window
+In function scope: gives window
+In method scope: gives object
+In event listner: gives whatever infront of .addEventListner
+*/
