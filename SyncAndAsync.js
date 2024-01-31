faee1d6ce155a49ca0f749368687eb8f6a624a78
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-01-31 22:11:40 +0545

    Understanding Sync And Async
    Understanding Sync And Async

---
+++ SyncAndAsync.js
@@ -0,0 +1,9 @@
+// Understanding Sync and Async 
+
+/*
+
+Sync: If multiple code runs one after another, after completing one task and going to another task, then it means Sync.
+
+Async: If multiple code runs at a time, and whose ever output pops up then we give reply to it, then it means Async.
+
+*/
