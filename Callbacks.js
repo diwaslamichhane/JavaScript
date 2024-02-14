1390dc43523f1b0c110d51c52c0c903fd16210b5
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-02-14 16:53:35 +0545

    Understanding Callbacks in JS
    Understanding Callbacks in JS

--- Callbacks.js
+++ Callbacks.js
@@ -4,3 +4,13 @@
 // Example:
 
 setTimeout(function(){},10000); //code runs after 10 seconds
+
+// Another Example:
+
+function sum(a, b){
+  console.log(a+b);
+}
+function calculator(a, b, sum){
+  sum(a, b)
+}
+calculator(1,2,sum) //sum is a callback function which is inside a parameter 
