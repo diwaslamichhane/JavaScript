9e66e35d7cde09af0e996e1231207521ab8eba35
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2024-02-15 20:28:46 +0545

    Understanding about promises
    Understanding about promises

---
+++ Promise.js
@@ -0,0 +1,17 @@
+// Understanding about promises
+
+// -It is an object
+// - It is the solution to callback hell
+
+// PROMISE
+// -pending
+// -fulfilles (Resolved)
+// -reject
+
+// example:
+
+let promise = new promise((resolve, reject) =>{
+    console.log("This is a promise.")
+    // resolve("Sucess");
+    // reject("Error Occured")
+});
